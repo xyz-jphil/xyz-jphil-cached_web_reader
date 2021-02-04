3bd0c02e15a4cc55295e48da8f4d84c65b635dc3
ivan-velikanova <ivan.velikanova@gmail.com>
2021-02-04 17:53:57 +0530

    Recording http status, and more information about the cache and source with timestamp

--- src/main/java/io/github/xyzjphil/xyz/jphil/cached_web_reader/ReadWeb.java
+++ src/main/java/io/github/xyzjphil/xyz/jphil/cached_web_reader/ReadWeb.java
@@ -22,6 +22,7 @@ import static java.nio.file.StandardOpenOption.*;
 import java.util.Optional;
 import java.util.function.Consumer;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -32,6 +33,43 @@ import org.jsoup.nodes.Document;
  * @author
  */
 public class ReadWeb {
+    private final String url;
+    private boolean refreshCache;
+    private Consumer<Exception> onException;
+    private Response response;
+    private Document cachedResult;
+    private boolean cache;
+    private boolean fresh;
+    private long dataFetchTimestamp;
+
+    public ReadWeb(String url) {
+        this.url = url;
+    }
+    
+    public Optional<Document> document(){
+        return readUrl(url, 0, refreshCache, onException);
+    }
+    
+    public boolean dataExists(){
+        return cachedResult!=null;
+    }
+
+    public boolean isCache() {
+        return cache;
+    }
+
+    public boolean isFresh() {
+        return fresh;
+    }
+
+    public java.util.Date dataFetchTimestamp() {
+        return new java.util.Date(dataFetchTimestamp);
+    }
+    
+    public static ReadWeb read(String link){
+        return new ReadWeb(link);
+    }
+    
     public static final Optional<Path> localCachePath(String url, Consumer<Exception> onException){
         Path uh = Paths.get(System.getProperty("user.home"));
         Path cache = uh.resolve(".jphil").resolve("xyz-jphil-cached_web_reader").resolve("cache");
@@ -61,6 +99,10 @@ public class ReadWeb {
     }
     
     public static final Optional<Document> readUrl(String url, int expectedMinSize, boolean refreshCache, Consumer<Exception> onException){
+        return readUrlImpl(url, expectedMinSize, refreshCache, onException, null);
+    }
+    private static final Optional<Document> readUrlImpl(String url, int expectedMinSize, 
+            boolean refreshCache, Consumer<Exception> onException, ReadWeb rw){
         url = url.replace("//", "/");
         url = url.replace("//", "/");
         url = url.replace("http:/", "http://"); //put back
@@ -71,10 +113,20 @@ public class ReadWeb {
             if(localCachePath!=null && Files.exists(localCachePath) 
                     && Files.size(localCachePath)>expectedMinSize && !refreshCache){ 
                 Document d = Jsoup.parse(localCachePath.toFile(), "UTF-8");
+                if(rw!=null){
+                    rw.cachedResult = d;
+                    rw.dataFetchTimestamp = Files.getLastModifiedTime(localCachePath).toMillis();
+                    rw.cache = true;
+                    rw.fresh = false;
+                }
                 return Optional.of(d);
             }
             System.out.println("Reading from web "+url);
-            String html = Request.Get(url).execute().returnContent().asString();
+            Response response = Request.Get(url).execute();
+            if(rw!=null){
+                rw.response = response;
+            }
+            String html = response.returnContent().asString();
             Document d = Jsoup.parse(html);
             try {
                 Files.writeString(localCachePath, html, WRITE, CREATE);
@@ -82,7 +134,12 @@ public class ReadWeb {
                 if(onException!=null)onException.accept(e);
                 else e.printStackTrace();
             }
-
+            if(rw!=null){
+                rw.cachedResult = d;
+                rw.dataFetchTimestamp = System.currentTimeMillis();
+                rw.cache = false;
+                rw.fresh = true;
+            }
             return Optional.of(d);
         } catch (Exception e) {
             if(onException!=null)onException.accept(e);
